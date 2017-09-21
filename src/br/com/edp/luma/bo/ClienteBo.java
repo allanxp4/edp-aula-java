bbeabbd6412211d82abd7b4f5aa8a7d7d640a9b2
logonrm <logonrm@fiap.com.br>
2017-09-21 20:10:39 -0300

    Cliente

--- src/br/com/edp/luma/bo/ClienteBo.java
+++ src/br/com/edp/luma/bo/ClienteBo.java
@@ -1,5 +1,77 @@
 package br.com.edp.luma.bo;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.edp.luma.beans.Cliente;
+import br.com.edp.luma.dao.ClienteDao;
+
 public class ClienteBo {
+	ClienteDao dao;
 	
-}
+	public ClienteBo() throws Exception{
+		dao = new ClienteDao();
+	}
+	
+	public String novoCliente(Cliente cliente)throws Exception{
+		if(cliente.getNome().length()>50){
+			return "Nome execedeu a quantidade de caracteres";
+		}
+		try{
+		cliente.setNome(cliente.getNome().toUpperCase());
+		String x = dao.adicionar(cliente);
+		dao.fechar();
+		return x;
+		} catch (SQLException e){
+			return null;
+	}
+	}
+	public String atualizarCliente(Cliente cliente)throws Exception{
+		try{
+			cliente.setNome(cliente.getNome().toUpperCase());
+			String x = dao.atualizar(cliente);
+			dao.fechar();
+			return x;
+		}
+		catch(SQLException e){	
+		}
+		return null;
+	}
+			
+		public String removerCliente(Cliente cliente)throws Exception{
+			try{
+				cliente.setNome(cliente.getNome().toUpperCase());
+				String x = dao.apagar(cliente);
+				dao.fechar();
+				return x;
+			}
+			catch(SQLException e){	
+			}
+			return null;
+		}
+		
+		public Cliente buscarCliente(int id)throws Exception{
+			try{
+				Cliente x = dao.getCliente(id);
+				dao.fechar();
+				return x;
+				
+			}
+			catch(SQLException e){
+				
+			}
+			return null;
+		}
+		
+		public List<Cliente> getTodosClientes(){
+			try{
+				return dao.getClientes();
+			}
+			catch(SQLException e){
+				
+			}
+			return null;
+		}
+	
+		
+	}
--- src/br/com/edp/luma/dao/ClienteDao.java
+++ src/br/com/edp/luma/dao/ClienteDao.java
@@ -1,11 +1,103 @@
 package br.com.edp.luma.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.edp.luma.beans.Cliente;
+import br.com.edp.luma.beans.Local;
+import br.com.edp.luma.connection.ConnectionFactory;
 
 public class ClienteDao {
-	private Connection conn;
+	private Connection con;
 	
 	public ClienteDao() throws Exception {
-		//conn =  ConexaoFactory.retornarConexao();
+		con =  ConnectionFactory.getConnection();
+	}
+	public void fechar()throws Exception{
+		con.close();
+	}
+	
+	public String adicionar(Cliente cliente) throws SQLException{
+		String sql = "INSERT INTO cliente (id, nome, email, telefone, cpf, login, senha, local)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement estrutura = con.prepareStatement(sql);
+		estrutura.setInt(1, cliente.getId());
+		estrutura.setString(2, cliente.getNome());
+		estrutura.setString(3, cliente.getEmail());
+		estrutura.setString(4, cliente.getTelefone());
+		estrutura.setString(5, cliente.getCpf());
+		estrutura.setString(6, cliente.getLogin());
+		estrutura.setString(7, cliente.getSenha());
+		estrutura.setInt(8, Local.getId());
+		int x = estrutura.executeUpdate();
+		estrutura.close();
+		return x + " cliente(s) foi(ram) adicionado(s)";
+	}
+	
+	public String atualizar(Cliente cliente) throws SQLException{
+		String sql = "UPDATE cliente (id, nome, email, telefone, cpf, login, senha, local)"
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement estrutura = con.prepareStatement(sql);
+		estrutura.setInt(1, cliente.getId());
+		estrutura.setString(2, cliente.getNome());
+		estrutura.setString(3, cliente.getEmail());
+		estrutura.setString(4, cliente.getTelefone());
+		estrutura.setString(5, cliente.getCpf());
+		estrutura.setString(6, cliente.getLogin());
+		estrutura.setString(7, cliente.getSenha());
+		estrutura.setInt(8, Local.getId());
+		int x = estrutura.executeUpdate();
+		estrutura.close();		
+		return x + " cliente(s) foi(ram) atualizado(s)";
+
+	}
+	public String apagar(Cliente cliente) throws SQLException{
+		String sql = "DELETE FROM cliente WHERE id = ?";
+		PreparedStatement estrutura = con.prepareStatement(sql);
+		int x = estrutura.executeUpdate();
+		estrutura.close();
+		return x + "cliente(s) foi(ram) apagado(s)";
+	}
+	public Cliente getCliente(int id) throws SQLException{
+		String sql = "SELECT * FROM cliente WHERE id = ?";
+		PreparedStatement estrutura = con.prepareStatement(sql);
+		estrutura.setInt(1, id);
+		ResultSet set = estrutura.executeQuery();
+	
+		Cliente cliente = new Cliente();
+		if(set.next()){
+			cliente.setId(set.getInt("id"));
+			cliente.setNome(set.getString("nome"));
+			cliente.setEmail(set.getString("email"));
+			cliente.setTelefone(set.getString("telefone"));
+			cliente.setCpf(set.getString("cpf"));
+			Local.setId(set.getInt("local Id"));
+		}
+		estrutura.close();
+		set.close();
+		return cliente;
+	}
+		public List<Cliente> getClientes() throws SQLException{
+			String sql = "SELECT * FROM clientes";
+			PreparedStatement estrutura = con.prepareStatement(sql);
+			ResultSet set = estrutura.executeQuery();
+		
+			List<Cliente> clientes = new ArrayList<>();
+			while(set.next()){
+				Cliente cliente = new Cliente();
+				cliente.setId(set.getInt("id"));
+				cliente.setNome(set.getString("nome"));
+				cliente.setEmail(set.getString("email"));
+				cliente.setTelefone(set.getString("telefone"));
+				cliente.setCpf(set.getString("cpf"));
+				Local.setId(set.getInt("local id"));
+				clientes.add(cliente);
+			}
+		
+		return clientes;
 	}
 }
