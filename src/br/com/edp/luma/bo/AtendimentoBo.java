6444d6dbdb8a9f511b0214a624272e6ca07bea95
logonrm <logonrm@fiap.com.br>
2017-09-21 20:44:22 -0300

    Atendimento

--- src/br/com/edp/luma/bo/AtendimentoBo.java
+++ src/br/com/edp/luma/bo/AtendimentoBo.java
@@ -1,5 +1,61 @@
 package br.com.edp.luma.bo;
 
-public class AtendimentoBo {
+import java.sql.SQLException;
+import java.util.List;
 
+import br.com.edp.luma.beans.Atendimento;
+import br.com.edp.luma.dao.AtendimentoDao;
+
+public class AtendimentoBo {
+	AtendimentoDao dao;
+	
+	public AtendimentoBo() throws Exception{
+		dao = new AtendimentoDao();
+	}
+	
+	public boolean adicionarAtendimento(Atendimento atendimento){
+		try {
+			return dao.adicionar(atendimento);
+		} catch (SQLException e) {
+		}
+		return false;
+	}
+	
+	public boolean editarAtendimento(Atendimento atendimento){
+		try {
+			return dao.adicionar(atendimento);
+		} catch (SQLException e) {	
+		}
+		return false;
+	}
+	
+	public boolean removerAtendimento(Atendimento atendimento){
+		try{
+			return dao.apagar(atendimento);
+		}
+		catch(SQLException e){
+			
+		}
+		return false;
+	}
+	
+	public Atendimento buscarAtendimento(int id){
+		try{
+			return dao.getAtendimento(id);
+		}
+		catch(SQLException e){
+			
+		}
+		return null;
+	}
+	
+	public List<Atendimento> getTodosAtendimentos(){
+		try{
+			return dao.getAtendimentos();
+		}
+		catch(SQLException e){
+			
+		}
+		return null;
+	}
 }
