8e708e77f683a1a985c1e34f7f3423d29c654483
Allan Araujo <allanxp4@gmail.com>
2017-09-22 20:00:28 -0300

    victor - usertype (e refatoraçao)

--- src/br/com/edp/luma/beans/UserType.java
+++ src/br/com/edp/luma/beans/UserType.java
@@ -1,5 +1,28 @@
 package br.com.edp.luma.beans;
 
 public class UserType {
-
+	private int userTypeId;
+	private String userType;
+	
+	public UserType(int userTypeId, String userType) {
+		super();
+		this.userTypeId = userTypeId;
+		this.userType = userType;
+	}
+	public UserType() {
+		super();
+	}
+	
+	public int getUserTypeId() {
+		return userTypeId;
+	}
+	public void setUserTypeId(int userTypeId) {
+		this.userTypeId = userTypeId;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
 }
