00b161c7f5cc4fbe63cc147ecd56e6935bb03dd5
Allan Araujo <allanxp4@gmail.com>
2017-09-22 21:05:51 -0300

    bo rodrigo

--- src/br/com/edp/luma/bo/UserPermissionBo.java
+++ src/br/com/edp/luma/bo/UserPermissionBo.java
@@ -1,44 +1,43 @@
 package br.com.edp.luma.bo;
 
-import java.sql.SQLException;
-import java.util.Arrays;
-
 import br.com.edp.luma.beans.UserPermission;
 import br.com.edp.luma.dao.UserPermissionDao;
 
 public class UserPermissionBo {
-
-
 	
-	public UserPermission avaliar(int id_user, String tipo_pessoa) throws Exception{
+	
+	
+	public boolean avaliar(UserPermission permission) throws Exception{
 		
 		UserPermission userPermission = new UserPermission();
 		
-		//Valida��o ID
-		if(id_user <=99999 && id_user >= 1000000){
-			userPermission.setNivelPermissao(-1);
-			userPermission.setTipoPessoa("Inv�lido");
-			userPermission.setUserId(-1);
-			return userPermission;
-		}
-		if(tipo_pessoa == ""){
-			userPermission.setNivelPermissao(-1);
-			userPermission.setTipoPessoa("Inv�lido");
-			userPermission.setUserId(-1);
-			return userPermission;
-		}
-		
-		public boolean adicionarPermissao(UserPermission permissao) throws SQLExceptin{
-			if()
-		}
-		
-		
-		
-		
 		UserPermissionDao dao = new UserPermissionDao();
-		UserPermission x = dao.avaliar(id_user, tipo_pessoa);
-		dao.fechar();
-		return x;
+		return dao.avaliar(permission);
 		
 	}
-}
+		
+		public boolean adicionarPermissao(UserPermission permissao) throws Exception{
+			UserPermissionDao dao = new UserPermissionDao();
+			
+			if(permissao != null){
+				return dao.adicionarPermissao(permissao);
+			}
+			return false;
+		
+		}
+		
+		public boolean removerPermissao(UserPermission permissao) throws Exception{
+			UserPermissionDao dao = new UserPermissionDao();
+			
+			if(permissao != null){
+				return dao.removerPermissao(permissao);
+			}
+			return false;
+		
+		}
+		
+		
+	
+		
+	}
+
--- src/br/com/edp/luma/dao/UserPermissionDao.java
+++ src/br/com/edp/luma/dao/UserPermissionDao.java
@@ -2,44 +2,28 @@ package br.com.edp.luma.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import br.com.edp.luma.beans.UserPermission;
 import br.com.edp.luma.connection.ConnectionFactory;
 
-public class UserPermissionDao{
+public class UserPermissionDao {
 	private Connection conn;
-	
-	
-	public UserPermissionDao() throws Exception{
-		conn = ConnectionFactory.getConnection();
+
+		public UserPermissionDao() throws Exception{
+			conn = ConnectionFactory.getConnection();
 			
 		}
-	public void fechar() throws Exception{
-		conn.close();
-	}
-		public UserPermission avaliar(int id_user, String tipo_pessoa) throws SQLException{
-			UserPermission permissao = new UserPermission();
-			String sql = "SELECT ID FROM USERPERMISSIONS WHERE ID = ? AND TIPO_PESSOA =?";
+		public boolean avaliar(UserPermission permissao) throws SQLException{
+			String sql = "SELECT ID FROM USER_PERMISSIONS WHERE ID = ? AND TIPO_PESSOA =?";
 			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setInt(1, id_user);
-			stmt.setString(2, tipo_pessoa);
-			
-			ResultSet resultado = stmt.executeQuery();
-			
-			if(resultado.next()){
-				permissao.setNivelPermissao(resultado.getInt("TIPO_PERMISSAO"));
-				permissao.setTipoPessoa(resultado.getString("TIPO_PESSOA"));
-				permissao.setUserId(resultado.getInt("ID"));
-			}
-			
-			return permissao;
-			
+			stmt.setInt(1, permissao.getUserId());
+			stmt.setString(2, permissao.getTipoPessoa());
+			return stmt.execute();
 		}
 	
 		public boolean adicionarPermissao(UserPermission permissao) throws SQLException{
-			String sql = "INSERT INTO USERPERMISSIONS (TIPO_PERMISSAO, TIPO_PESSOA)"
+			String sql = "INSERT INTO USER_PERMISSIONS (TIPO_PERMISSAO, TIPO_PESSOA)"
 					+ "VALUES (?, ?)";;
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, permissao.getNivelPermissao());
@@ -48,7 +32,7 @@ public class UserPermissionDao{
 		}
 		
 		public boolean alterarPermissao(UserPermission permissao) throws SQLException{
-			String sql = "UPDATE USERPERMISSIONS SET TIPO_PERMISSAO = ?"
+			String sql = "UPDATE USER_PERMISSIONS SET TIPO_PERMISSAO = ?"
 					+ " WHERE ID = ? AND TIPO_PESSOA = ?";
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setInt(2, permissao.getUserId());
@@ -56,6 +40,14 @@ public class UserPermissionDao{
 			stmt.setString(3, permissao.getTipoPessoa());
 			return stmt.execute();
 		}
+		
+		public boolean removerPermissao(UserPermission permissao) throws SQLException{
+			String sql = "DELETE FROM USER_PERMISSIONS WHERE ID = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, permissao.getUserId());
+			return stmt.execute();
+		}
+		
 		
 		
 }
